8f628acfea1dd7f092c1bf52361f6cd2cd34c737
akshith <akki312>
2024-05-14 11:50:49 +0530

    book.js

---
+++ models/book.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  },
+  ISBN: {
+    type: String,
+    required: true
+  },
+  availability: {
+    type: Boolean,
+    default: true
+  }
+});
+
+
+module.exports = mongoose.model('Book', bookSchema);
