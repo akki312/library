ec5d31bf7782b4a5a712f98491636bd3d6a85427
akki312 <akshithsistla@gmail.com>
2024-05-16 19:09:45 +0530

    index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,4 @@
 const app = (module.exports = require('express'))
 
-app.get("/", (req, res) => {
-    res.send({ msg: "library serviceis up and running" });
-  });
+
 app.use('/books', require('../routes/library'))
