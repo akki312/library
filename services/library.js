dc4f8060119bdaebc5be55b64c608cae3dac1a49
akki312 <akshithsistla@gmail.com>
2024-05-20 09:36:29 +0530

    library.js

--- services/library.js
+++ services/library.js
@@ -4,7 +4,14 @@ const logger = require("../loaders/logger")
 async function createBooks(data) {
   try {
     logger.info("inserting  the books data");
-    let newBook = await new bookModel(data);
+    let newBook = await bookModel({
+      ISBN: "ISBN9992",
+      author: "backed",
+      title: "something",
+      availability: "true"
+    }
+
+    );
     let dbResponse = await newBook.save();
     console.log(dbResponse);
     return true;
