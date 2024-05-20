0c58536d876b1d542fae72b4f70c32985550867c
akki312 <akshithsistla@gmail.com>
2024-05-20 10:24:38 +0530

    book.js

--- models/book.js
+++ models/book.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
 const bookSchema = new Schema({
   ISBN: { type: String, required: true },
   author: { type: String, required: true },
   title: { type: String, required: true },
-  availabilty : {type: Boolean}
+  availabilty : {type: Boolean, default: true}
   // Other fields can go here
 });
 
